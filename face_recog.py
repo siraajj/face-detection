859e64d2743c3fd4c1defacaf3aa3388bb23575e
siraajj <sirajudeenthadathil@gmail.com>
2025-03-12 12:00:12 +0530

    Add files via upload

---
+++ face_recog.py
@@ -0,0 +1,21 @@
+import cv2
+face_classifier=cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+video_capture=cv2.VideoCapture(0)
+def detetct_bounding_box(vid):
+    gray_image=cv2.cvtColor(vid,cv2.COLOR_BGR2GRAY)
+    faces=face_classifier.detectMultiScale(gray_image,1.1,5,minSize=(40,40))
+    for (x,y,w,h)in faces:
+        cv2.rectangle(vid,(x,y),(x+w,y+h),(0,255,0),4)
+    return faces
+while True:
+    result,video_frame=video_capture.read()
+    if result is False:
+        break
+    faces=detetct_bounding_box(video_frame)
+    cv2.imshow('my face detection',video_frame)
+
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
